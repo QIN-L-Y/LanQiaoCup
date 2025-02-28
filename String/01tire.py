8785e3a762b6a1a90dd649f2ae9ccd4a5a9210fd
EtherealTide <22371160@buaa.edu.cn>
2025-02-28 22:35:39 +0800

    Update 01tire.py

--- String/01tire.py
+++ String/01tire.py
@@ -1,14 +1,38 @@
 # https://www.lanqiao.cn/problems/8616/learning/
-class Tree:
+class Tree():
     def __init__(self):
         self.children = {}
-        self.isleaf = False
+        self.isleaf = 0
 
-    def insert(self, arr):
+    def insert(self, number): # 用于构建01tire树
         node = self
         for i in range(32):
-            key = arr << (31 - i) & 1
+            key = number << (31 - i) & 1 # 从32位开始存入，防止各数的二进制长度不一致
             if key not in node.children.keys():
                 node.children[key]
             node = node.children[key]
-        node.isleaf = True
+        node.isleaf = number
+
+    def search(self,number): # 用于搜索最大异或和
+        node=self
+        for i in range(32):
+            binary= number << (31 - i) & 1
+            if binary^1 in node.children.keys():
+                node=node.children[binary^1]
+            else:
+                node=node.children[binary]
+        return node.isleaf
+
+n=int(input())
+arr=list(map(int,input().split()))
+root=Tree()
+for num in arr:
+    root.insert(num)
+q=int(input())
+ans=[]
+for i in range(q):
+    x=int(input())
+    ans.append(root.search(x)^x)
+for i in range(q):
+    print(ans[i])
+    print()
