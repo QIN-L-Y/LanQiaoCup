c6a61d715e1c6671e5be846df795d04a667b18a0
QIN-L-Y <22371160@buaa.edu.cn>
2025-02-28 22:48:06 +0800

    refine

--- String/01tire.py
+++ String/01tire.py
@@ -1,38 +1,39 @@
 # https://www.lanqiao.cn/problems/8616/learning/
-class Tree():
+class Tree:
     def __init__(self):
         self.children = {}
         self.isleaf = 0
 
-    def insert(self, number): # 用于构建01tire树
+    def insert(self, number):  # 用于构建01tire树
         node = self
         for i in range(32):
-            key = number << (31 - i) & 1 # 从32位开始存入，防止各数的二进制长度不一致
-            if key not in node.children.keys():
-                node.children[key]
+            key = (number >> (31 - i)) & 1  # 从32位开始存入，防止各数的二进制长度不一致
+            if key not in node.children:
+                node.children[key] = Tree()  # 初始化子节点
             node = node.children[key]
         node.isleaf = number
 
-    def search(self,number): # 用于搜索最大异或和
-        node=self
+    def search(self, number):  # 用于搜索最大异或和
+        node = self
         for i in range(32):
-            binary= number << (31 - i) & 1
-            if binary^1 in node.children.keys():
-                node=node.children[binary^1]
+            binary = (number >> (31 - i)) & 1
+            if binary ^ 1 in node.children:
+                node = node.children[binary ^ 1]
             else:
-                node=node.children[binary]
+                node = node.children[binary]
         return node.isleaf
 
-n=int(input())
-arr=list(map(int,input().split()))
-root=Tree()
+
+n = int(input())
+arr = list(map(int, input().split()))
+root = Tree()
 for num in arr:
     root.insert(num)
-q=int(input())
-ans=[]
+q = int(input())
+ans = []
 for i in range(q):
-    x=int(input())
-    ans.append(root.search(x)^x)
+    x = int(input())
+    ans.append(root.search(x) ^ x)
 for i in range(q):
     print(ans[i])
-    print()
+    print()  # 用于换行
