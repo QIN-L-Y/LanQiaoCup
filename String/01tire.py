87ab8fa2135e4fbbfff924d512cc8fa53648cca3
EtherealTide <22371160@buaa.edu.cn>
2025-02-28 22:12:53 +0800

    Update 01tire.py

--- String/01tire.py
+++ String/01tire.py
@@ -1,1 +1,16 @@
 # https://www.lanqiao.cn/problems/8616/learning/
+class Tree():
+    def __init__(self):
+        self.children={}
+        self.isleaf=False
+
+    def insert(self, arr):
+        node=self
+        for i in range(32):
+            key=arr<<(31-i)&1
+            if key not in  node.children.keys()
+                node.children[key]
+            node=node.children[key]
+        node.isleaf=True
+
+    def
