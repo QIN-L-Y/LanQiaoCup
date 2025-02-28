4653c34b75d7f4b5e2c2f0add718c48d049e343a
QIN-L-Y <22371160@buaa.edu.cn>
2025-02-28 22:14:05 +0800

    refine

--- String/01tire.py
+++ String/01tire.py
@@ -1,16 +1,14 @@
 # https://www.lanqiao.cn/problems/8616/learning/
-class Tree():
+class Tree:
     def __init__(self):
-        self.children={}
-        self.isleaf=False
+        self.children = {}
+        self.isleaf = False
 
     def insert(self, arr):
-        node=self
+        node = self
         for i in range(32):
-            key=arr<<(31-i)&1
-            if key not in  node.children.keys()
+            key = arr << (31 - i) & 1
+            if key not in node.children.keys():
                 node.children[key]
-            node=node.children[key]
-        node.isleaf=True
-
-    
+            node = node.children[key]
+        node.isleaf = True
