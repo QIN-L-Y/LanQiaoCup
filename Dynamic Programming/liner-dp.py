c780c0a05580abfc995950dae38e1c55c0581231
EtherealTide <22371160@buaa.edu.cn>
2025-03-01 15:18:29 +0800

    Update liner-dp.py

--- Dynamic Programming/liner-dp.py
+++ Dynamic Programming/liner-dp.py
@@ -1,1 +1,11 @@
 # https://www.lanqiao.cn/problems/3423/learning/
+n,k=map(int,input().split())
+def dp():
+    ans=[]
+    ans.append(1)
+    for i in range(n):
+        if i-k-1>=0:
+            ans.append(ans[i-k-1]+ans[i-1])
+        else:
+            ans.append(ans[i-1]+1)
+print(dp())
