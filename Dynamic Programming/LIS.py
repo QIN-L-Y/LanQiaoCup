01e17d05df7885ff0bdde2be51a504264b20d63b
QIN-L-Y <22371160@buaa.edu.cn>
2025-03-01 17:48:40 +0800

    refine

--- Dynamic Programming/LIS.py
+++ Dynamic Programming/LIS.py
@@ -4,17 +4,19 @@ height_array = list((map(int, input().split())))
 
 
 def dynamic_programming():
-    for i in range(1, n):
+    for i in range(1, n):  # 寻找以i结尾的最长上升子序列
         for j in range(i):
             if dp[j] + 1 > dp[i] and height_array[j] < height_array[i]:
                 dp[i] = dp[j] + 1
-    for i in range(n - 1, -1, -1):
+    for i in range(n - 1, -1, -1):  # 寻找以i开头的最长上升子序列
         for j in range(n - 1, i, -1):
             if dp2[j] + 1 > dp2[i] and height_array[j] < height_array[i]:
                 dp2[i] = dp2[j] + 1
-    return max(dp[i] + dp2[i] for i in range(n)) - 1
+    return (
+        max(dp[i] + dp2[i] for i in range(n)) - 1
+    )  # 为什么要减1，因为i这个点在长度中被计算了两次
 
 
-dp = [1] * n
-dp2 = [1] * n
+dp = [1] * n  # 记录从左往右的最长上升子序列
+dp2 = [1] * n  # 记录从右往左的最长上升子序列
 print(n - dynamic_programming())
