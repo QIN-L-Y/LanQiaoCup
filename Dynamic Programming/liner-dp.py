2346e318ad7d709e0124df9fc99e5fab8151ab3a
QIN-L-Y <22371160@buaa.edu.cn>
2025-03-01 15:31:31 +0800

    refine

--- Dynamic Programming/liner-dp.py
+++ Dynamic Programming/liner-dp.py
@@ -1,11 +1,18 @@
 # https://www.lanqiao.cn/problems/3423/learning/
-n,k=map(int,input().split())
+n, k = map(int, input().split())
+mod = 1e9 + 7
+
+
 def dp():
-    ans=[]
-    ans.append(1)
-    for i in range(n):
-        if i-k-1>=0:
-            ans.append(ans[i-k-1]+ans[i-1])
+    ans = []
+    ans.append(2)
+    for i in range(1, n):
+        if i - k - 1 >= 0:
+            ans.append((ans[i - k - 1] + ans[i - 1]) % mod)
         else:
-            ans.append(ans[i-1]+1)
-print(dp())
+            ans.append((ans[i - 1] + 1) % mod)
+
+    return ans[-1]
+
+
+print(int(dp()))
