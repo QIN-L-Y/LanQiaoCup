02957ba81762cbbd51ee36a54adf2d104f24c9f0
QIN-L-Y <22371160@buaa.edu.cn>
2025-03-01 16:29:00 +0800

    refine

--- Dynamic Programming/LIS.py
+++ Dynamic Programming/LIS.py
@@ -1,16 +1,19 @@
 # https://www.lanqiao.cn/problems/742/learning/
-n=int(input())
-height_array=[]
+n = int(input())
+height_array = []
 for i in range(n):
     height_array.append(int(input))
+
+
 def dynamic_programming():
-    for i n range(n):
+    for i in range(n):
         for j in range(n):
-            if j==i:
+            if j == i:
                 continue
             else:
-                if dp[j]>dp[i] and height_array[j]<height_array[i]:
-                    dp[i]=dp[j]+1
+                if dp[j] > dp[i] and height_array[j] < height_array[i]:
+                    dp[i] = dp[j] + 1
 
-dp=[0]*n
-print(n-max(dp))
+
+dp = [0] * n
+print(n - max(dp))
