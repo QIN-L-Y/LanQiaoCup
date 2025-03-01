5743e8d6c4ed9245ca5c088b42303d99d1c92e51
QIN-L-Y <22371160@buaa.edu.cn>
2025-03-01 12:18:16 +0800

    refine

--- String/01trie.py
+++ String/01trie.py
@@ -1,14 +1,14 @@
 # https://www.lanqiao.cn/problems/8616/learning/
 class Tree:
     def __init__(self):
-        self.children = {}
+        self.children = [None, None]
         self.isleaf = 0
 
-    def insert(self, number):  # 用于构建01tire树
+    def insert(self, number):  # 用于构建01trie树
         node = self
         for i in range(32):
             key = (number >> (31 - i)) & 1  # 从32位开始存入，防止各数的二进制长度不一致
-            if key not in node.children:
+            if node.children[key] is None:  # 如果子节点不存在，就新建一个子节点
                 node.children[key] = Tree()  # 初始化子节点
             node = node.children[key]
         node.isleaf = number
@@ -17,7 +17,7 @@ class Tree:
         node = self
         for i in range(32):
             binary = (number >> (31 - i)) & 1
-            if binary ^ 1 in node.children:
+            if node.children[binary ^ 1] is not None:
                 node = node.children[binary ^ 1]
             else:
                 node = node.children[binary]
